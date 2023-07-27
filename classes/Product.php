743226c65f03f6d86cfcf811b72e86576bac86b0
Waruna <warunahello@gmail.com>
2023-07-27 21:05:38 +0530

    product update part added

--- classes/Product.php
+++ classes/Product.php
@@ -58,12 +58,57 @@
         }
 
 
-        public function getCatById($id){
-            
+        public function getProById($id){
+            $query="SELECT * FROM Product WHERE ProductID='$id'";
+            $result=$this->db->select($query);
+            return $result;
         }
 
-        public function catUpdate($catName,$id){
-           
+        public function productUpdate($data,$file,$id){
+            $productName=mysqli_real_escape_string($this->db->link,$data['name']);
+            $catId=mysqli_real_escape_string($this->db->link,$data['CategoryId']);
+            $description=mysqli_real_escape_string($this->db->link,$data['description']);
+            $price=mysqli_real_escape_string($this->db->link,$data['price']);
+            $stock=mysqli_real_escape_string($this->db->link,$data['qty']);
+            $size=mysqli_real_escape_string($this->db->link,$data['size']);
+
+            $premited=array('jpg','jpeg','png','gif');
+            $file_name=$_FILES['image']['name'];
+            $file_size=$_FILES['image']['size'];
+            $file_temp=$_FILES['image']['tmp_name'];
+
+            $div=explode('.',$file_name);
+            $file_ext=strtolower(end($div));
+            $unique_image=substr(md5(time()),0,10).'.'.$file_ext;
+            $uploaded_image="upload/".$unique_image;
+
+            if ($productName == "" || $catId=="" ||  $description=="" || $price=="" || $stock=="" || $size=="" ) {
+                $msg="Fields must not be empty !";
+                return $msg;
+            }else{
+                move_uploaded_file($file_temp,$uploaded_image);
+                 $query=
+                 "UPDATE Product 
+                    SET 
+                    CategoryId='$catId',
+                    Name='$productName',
+                    Description='$description',
+                    Image='$uploaded_image',
+                    Price='$price',
+                    Stock='$stock',
+                    Size='$size'
+                    WHERE ProductID='$id';
+                 ";
+
+                $productupdate=$this->db->update($query);
+                if ($productupdate) {
+                    $msg="Product Update Successfully";
+                    return $msg;
+                }else{
+                    $msg="Product Not updated";
+                    return $msg;
+                }
+            }
         }
 
 
